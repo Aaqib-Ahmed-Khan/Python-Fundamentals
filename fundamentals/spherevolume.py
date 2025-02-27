e58046c61c4022426e04cfc460903090ab744739
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-02-27 22:43:34 +0500

    somelpgic programs

---
+++ fundamentals/spherevolume.py
@@ -0,0 +1,8 @@
+
+# radius=float(input("enter radius of sphere: " ))
+# volume =(4/3)*3.142 * (radius **3)
+# print("volume of sphere is ",volume)
+
+# celsius_temp=float(input("enter temp in celsius"))
+# fah_temp=(celsius_temp * 9/5)+32
+# print("temperature in fahrenheit is",fah_temp)
