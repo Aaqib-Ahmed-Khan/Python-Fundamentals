26e248cab57f382ce53227d432ac6f4b85423eb1
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-02-27 23:11:52 +0500

    profit or loss program

--- fundamentals/fundamentals.py
+++ fundamentals/fundamentals.py
@@ -1,17 +1,17 @@
-# name ="isha"
-# print (type(name))
+name ="isha"
+print (type(name))
 
-# char = "a"
-# print(ord(char))
-# name=input("enter your naem ")
-# print(name)
+char = "a"
+print(ord(char))
+name=input("enter your naem ")
+print(name)
 
-# age=int(input("enter you age"))
-# print(type(age))
+age=int(input("enter you age"))
+print(type(age))
 
-# number1=int(input("enter numbr 1:"))
+number1=int(input("enter numbr 1:"))
 
-# number2=int(input("enter numbr 2:"))
-# sum=number1+number2
-# print("sum o ftwo numebrs is ",sum)
+number2=int(input("enter numbr 2:"))
+sum=number1+number2
+print("sum o ftwo numebrs is ",sum)
 
