5ab6360f23b7d23be851caf89824780bb058872f
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-02-28 17:11:15 +0500

    insert,append,extend

---
+++ apend_ins_exten.py
@@ -0,0 +1,9 @@
+fruits = ["apple","mango","cherry"]
+fruits.append("kiwi")
+print(fruits)
+fruits.insert(3,"banana")
+print(fruits)
+
+more_fruits=["grapes","papaya"]
+fruits.extend(more_fruits)
+print(fruits)
