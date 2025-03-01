db63a32b7880d7cea6d65e6a01e59ce93228b23a
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-03-02 00:58:07 +0500

    sets

---
+++ set.py
@@ -0,0 +1,9 @@
+names={"aaqib","ahmed","khan"}
+# print(names)
+# for x in names:
+#     print(x)
+# if "ahmed" in names:
+#    print("ahmed is in set")
+names2={"ahmed","khan","khan2"}
+names4=names.union(names2)
+print(names4)
