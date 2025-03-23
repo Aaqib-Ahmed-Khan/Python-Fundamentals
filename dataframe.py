47cb07affcb86dd20eeacd304c2166205f216186
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-03-24 00:54:33 +0500

    using loc

--- dataframe.py
+++ dataframe.py
@@ -3,3 +3,8 @@ import pandas as pd
 data={"cal":[200,300,400],"duration":[50,40,45]}
 aaqib=pd.DataFrame(data)
 print(aaqib)
+# lcoate a specific row usjing attribute loc it retuetns onw or more specified row
+import pandas as pd
+data={"cal":[200,300,400],"duration":[40,45,50]}
+aaqib=pd.DataFrame(data)
+print(aaqib.loc[1])
