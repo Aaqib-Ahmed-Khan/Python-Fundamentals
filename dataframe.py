a75c75a7a3c0646e7ddce56f95a7237286a5c415
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-03-26 00:09:59 +0500

    2d tabular form

--- dataframe.py
+++ dataframe.py
@@ -3,8 +3,14 @@ import pandas as pd
 data={"cal":[200,300,400],"duration":[50,40,45]}
 aaqib=pd.DataFrame(data)
 print(aaqib)
-# lcoate a specific row usjing attribute loc it retuetns onw or more specified row
+# locate a specific row using attribute loc it retuetns onw or more specified row
 import pandas as pd
 data={"cal":[200,300,400],"duration":[40,45,50]}
 aaqib=pd.DataFrame(data)
 print(aaqib.loc[1])
+
+# example of returning row 0 and 1
+import pandas as pd
+data={"cal":[200,300,400],"duration":[40,45,50]}
+aaqib=pd.DataFrame(data)
+print(aaqib.loc[[0,1]])
